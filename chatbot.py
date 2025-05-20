4e21db66687c7d0563019d49b4c36d43407d39e0
Jazmin5465 <jazminguti06@gmail.com>
2025-05-20 14:15:24 -0500

    rearrange content

--- chatbot.py
+++ chatbot.py
@@ -1,24 +1,5 @@
 from openai import OpenAI
 
-# client = OpenAI()
-
-# user_input = input("\nAsk something for the chatbot: \n")
-
-# model = "gpt-3.5-turbo"
-# messages = [
-#     {"role":"system","content":"You are an assistant that always answers in the form of a poem."},
-#     {"role": "user", "content": user_input}
-# ]
-
-# response = client.chat.completions.create(
-#     model = model,
-#     messages = messages
-# )
-
-# response_for_user = response.choices[0].message.content
-
-# print(response_for_user)
-
 client = OpenAI()
 
 def set_user_input_category(user_input):
--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 from openai import OpenAI
 
 client = OpenAI()
+
+user_input = input("\nAsk something for the chatbot: \n")
+
+model = "gpt-3.5-turbo"
+messages = [
+    {"role":"system","content":"You are an assistant that always answers in the form of a poem."},
+    {"role": "user", "content": user_input}
+]
+
+response = client.chat.completions.create(
+    model = model,
+    messages = messages
+)
+
+response_for_user = response.choices[0].message.content
+
+print(response_for_user)
